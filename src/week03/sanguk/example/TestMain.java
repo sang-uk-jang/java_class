4b6eef9e16ea285550aaef433fe46351833dd26f
sang-uk-jang <tkddnr2266@konkuk.ac.kr>
2026-09-14 14:34:49 +0900

    week03 add example

--- src/week03/sanguk/example/TestMain.java
+++ src/week03/sanguk/example/TestMain.java
@@ -15,7 +15,48 @@ public class TestMain {
             default -> System.out.println("잘못된 입력입니다.");
         }
     }
+    static void example02(){
+        int num = 5;
+        for(int i=1; i<=num; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println("=================");
+        for(int i=1; i<=num; i++){
+            for(int j=1;j<=num-i; j++)
+                System.out.print(" ");
+            for(int j=1; j<=i; j++)
+                System.out.print("*");
+            System.out.println();
+        }
+        System.out.println("=================");
+        for(int i=1; i<=num; i++){
+            for(int j=1;j<=num-i; j++)
+                System.out.print(" ");
+            for(int j=1; j<=2*i-1; j++)
+                System.out.print("*");
+            System.out.println();
+        }System.out.println("=================");
+        for(int i=1; i<=num; i++){
+            for(int j=1;j<=num-i; j++)
+                System.out.print(" ");
+            for(int j=1; j<=2*i-1; j++)
+                System.out.print("*");
+            System.out.println();
+        }
+        System.out.println("=================");
+        for(int i=1; i<=num; i++){
+            for(int j=1;j<=Math.abs(i-3); j++)
+                System.out.print(" ");
+            for(int j=1; j<=num-(Math.abs(i-3)*2); j++)
+                System.out.print("*");
+            System.out.println();
+        }
+    }
     static void main() {
-        example01();
+        //example01();
+        example02();
     }
 }
